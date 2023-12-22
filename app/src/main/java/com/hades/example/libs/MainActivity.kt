67d8f58e27fba8b273b470c615cdc5470e5f1da1
yingvickycao <ying.vicky.cao@outlook.com>
2023-12-22 20:04:07 +0800

    refactor

--- app/src/main/java/com/hades/example/libs/MainActivity.kt
+++ app/src/main/java/com/hades/example/libs/MainActivity.kt
@@ -2,11 +2,17 @@ package com.hades.example.libs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.hades.example.android.lib.utils.ThemeUtils
 import com.hades.example.myapplication.R
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate: " + ThemeUtils.dp2px(this, 10f));
     }
 }
